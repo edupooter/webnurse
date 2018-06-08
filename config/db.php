15ab9c775f059d401cbfc07889880decab67cf70
edupooter <senac@duduzinho.com>
2018-06-08 18:26:29 -0300

    heroku

--- config/db.php
+++ config/db.php
@@ -9,6 +9,10 @@ return [
     // 'password' => '',
     // 'charset' => 'utf8',
 
-
+    // ClearDB MySQL - Heroku
+    'dsn' => 'mysql:host=us-cdbr-iron-east-04.cleardb.net;dbname=heroku_87e6a5098f9ef4f',
+    'username' => 'b15d1f8b71313d',
+    'password' => 'edf5d926',
+    'charset' => 'utf8',
 
 ];
