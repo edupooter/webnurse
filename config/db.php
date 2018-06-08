fbfdfc7572aeb6f2b02ba6e57cae970993b307db
edupooter <senac@duduzinho.com>
2018-06-08 18:27:31 -0300

    apagar histórico db.php

--- config/db.php
+++ config/db.php
@@ -3,16 +3,12 @@
 return [
     'class' => 'yii\db\Connection',
 
-    // // MySQL
-    // 'dsn' => 'mysql:host=localhost;dbname=webnursetcc',
-    // 'username' => 'root',
-    // 'password' => '',
-    // 'charset' => 'utf8',
-
-    // ClearDB MySQL - Heroku
-    'dsn' => 'mysql:host=us-cdbr-iron-east-04.cleardb.net;dbname=heroku_87e6a5098f9ef4f',
-    'username' => 'b15d1f8b71313d',
-    'password' => 'edf5d926',
+    // MySQL
+    'dsn' => 'mysql:host=localhost;dbname=webnursetcc',
+    'username' => 'root',
+    'password' => '',
     'charset' => 'utf8',
+
+
 
 ];
